6ccf89609ef2ce49d9a683f98e612ab12259f14f
Caio Salvador <caiosalvador96@gmail.com>
2016-11-10 02:58:20 -0200

    adiciona routes, rota para cadastro

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,24 @@
-var mainApp = angular.module("SadeApp", []);
+var mainApp = angular.module("SadeApp", ['ngRoute']);
+
+// configure our routes
+mainApp.config(function($routeProvider) {
+    $routeProvider
+
+    // route for the home page
+    .when('/', {
+        templateUrl : 'assets/html/main.html',
+        controller  : 'MainCtrl'
+    })
+
+    // route for the about page
+    .when('/cadastro', {
+        templateUrl : 'assets/html/cadastro.html',
+        controller  : 'MainCtrl'
+    })
+
+    // 404 not found
+    //.otherwise({  });
+});
 
 mainApp.controller("MainCtrl", function($scope){
 
