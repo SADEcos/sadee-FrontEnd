4b64f9c4b2723cec5b522fb7b706036dd39c882b
Caio Salvador <caiosalvador96@gmail.com>
2016-11-11 22:42:07 -0200

    adiciona slider do cadastro e login teste

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,11 +22,40 @@ mainApp.config(function($routeProvider) {
         controller  : 'MainCtrl'
     })
 
+    // route for the about page
+    .when('/login', {
+        templateUrl : 'assets/html/login-fake.html',
+        controller  : 'MainCtrl'
+    })
+
     // 404 not found
     //.otherwise({  });
 });
 
 mainApp.controller("MainCtrl", function($scope){
+
+    $scope.cadastro = 1;
+
+    $scope.cadastro_para2 = function()
+    {
+        $scope.cadastro = 2;
+
+        // With JQuery
+        $("#java").slider();
+        $("#java").on("slide", function(slideEvt) {
+            $("#javaSliderVal").text(slideEvt.value);
+        });
+
+        $("#php").slider();
+        $("#php").on("slide", function(slideEvt) {
+            $("#phpSliderVal").text(slideEvt.value);
+        });
+    }
+
+    $scope.cadastro_para1 = function()
+    {
+        $scope.cadastro = 1;
+    }
 
 
 
