669f2577c5cb02aa63e3297e697977779b79515a
Victor Rodrigues Matsuguma <ra118893@tramp.lab.ic.unicamp.br>
2016-11-18 20:14:27 -0200

    Adicionados migués pros logins

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,6 +16,10 @@ mainApp.config(function($routeProvider) {
         controller  : 'MainCtrl'
     })
 
+    .when('/error', {
+        templateUrl : 'assets/html/error.html',
+        controller  : 'MainCtrl'
+    })
     // route for the about page
     .when('/cadastro', {
         templateUrl : 'assets/html/cadastro.html',
@@ -28,11 +32,34 @@ mainApp.config(function($routeProvider) {
         controller  : 'MainCtrl'
     })
 
+    .when('/dev', {
+        templateUrl : 'assets/html/login-fake.html',
+        controller  : 'MainCtrl'
+    })
+
+    .when('/admin', {
+        templateUrl : 'assets/html/login-fake.html',
+        controller  : 'MainCtrl'
+    })
+
     // 404 not found
     //.otherwise({  });
 });
 
 mainApp.controller("MainCtrl", function($scope){
+
+    $scope.ChangePage = function(name){
+        if(name.includes("admin")){
+            window.location.replace("http://localhost:1414/#admin");
+        }
+        else if(name.includes("dev")){
+            window.location.replace("http://localhost:1414/#dev");
+        }
+        else{
+            window.location.replace("http://localhost:1414/#error");
+        }
+
+}
 
     $scope.cadastro = 1;
 
