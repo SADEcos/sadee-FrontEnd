6dd303eba4dd4db14fce9b2945d125af3f84a4ec
Victor Rodrigues Matsuguma <ra118893@queiroz.lab.ic.unicamp.br>
2016-12-02 21:50:24 -0200

    Adicionado id para o botão de submit

--- assets/js/main.js
+++ assets/js/main.js
@@ -151,6 +151,8 @@ mainApp.controller("MainCtrl", function($scope, $rootScope, $http){
         $rootScope.password = password;
     }
 
+    
+
     $scope.ChangePage = function(name){
         if(name.includes("admin")){
             window.location.replace("http://localhost:1414/#busca");
